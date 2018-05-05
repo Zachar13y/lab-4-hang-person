9367486f9a96689888638ccd0197c6402513ffc2
Zachar13y <zachar13y@gmail.com>
2018-05-04 17:16:09 -0700

    Revert "My turn to drive!"

--- app.js
+++ app.js
@@ -1,63 +1,35 @@
+'use-strict';
 
-    var car = null;
-    var selectedCar = null;
-    
+
+
+
 // This selects a random word to be guessed by user
-function loadWord(){
+function loadWord(){ 
+    // for(i = 0; i > 5; i++) {
         var listOfWords = ['Honda', 'BMW', 'Tesla', 'Mercedes', 'Audi', 'Porsche'];
         var index = getRandomIndex(listOfWords.length);
         console.log(index);
         
-        car = listOfWords[index];
+        var car = listOfWords[index];
         console.log('selected car is ' + car);
+        // removes car from array
+        // listOfWords.splice(index, 1);
         
         // returns randomly selected car
         console.log(words);
 
-        selectedCar = car.split("");
-        console.log('split value of selected car is' , selectedCar);
+        var selectedCar = car.split("");
+        console.log(selectedCar);
 
-        var p = document.getElementById('guess-word');
-        p.textContent = (blanks);
-
-       // var blanks = selectedCar;
-        //p. textContent = document.getElementById('blank-spaces');
+        var p = document.getElementById('car-letters');
+        p.textContent = (selectedCar);
 
 }
 
-//guess letter input box and button 
-    function guessLetter() {
+//testing pull request changes HEYYYYYY
 
-        var userguess = document.getElementById("guess-box").value;
-        document.getElementById("userguess").innerHTML = userguess;
-        console.log(userguess);
-        console.log('this is car being guessed' , car);
-        
-        if (userguess === "") {
-            alert('Please guess a letter');
-        }
-        if (car.includes(userguess)) {
-            correctGuess();
 
-        }
-        var p = document.getElementById('correct-guess');
-        p.textContent = (userguess);
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
-        function correctGuess(){
-        document.getElementById("userguess").innerHTML = 'Good guess';
-
-        }
-        
-
-        // var userguess = includes(selectedCar[i]);
-        
-        // if (userguess = selectedCar[i]) {
-        //     alert('Correct!');
-        
-        // console.log(userguess);
-        // }
-    }
-
-    function getRandomIndex(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
